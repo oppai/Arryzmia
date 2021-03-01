c3b1da5b44bade0945033308beaea9938e501258
oppai <hotsoup.h@gmail.com>
2021-03-02 00:32:25 +0900

    Fix cache logic

--- lib/arryzmia/cache.ex
+++ lib/arryzmia/cache.ex
@@ -1,13 +1,12 @@
 defmodule Arryzmia.Cache do
   @cache_name Arryzmia
-  @default_expired_time 10800 # 60 * 60 * 3
+  @default_expired_time :timer.seconds(10800) # 60 * 60 * 3
   defmacro cache(key, expired_time \\ @default_expired_time, do: yield) do
     quote do
       case Cachex.get(unquote(@cache_name), unquote(key)) do
         {:ok, nil} ->
           result = unquote(yield)
-          Cachex.put(unquote(@cache_name), unquote(key), result)
-          Cachex.expire(unquote(@cache_name), unquote(key), unquote(expired_time))
+          Cachex.put(unquote(@cache_name), unquote(key), result, ttl: unquote(expired_time))
           result
         {:ok, value} ->
           value
